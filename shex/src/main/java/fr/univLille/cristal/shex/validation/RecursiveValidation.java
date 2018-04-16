b753adb7dd566bf83fe5a67599aea4266628e1af
DUSART Jeremie <jeremie.dusart@inria.fr>
2018-04-16 14:49:25 +0200

    Removing unused import.

--- shex/src/main/java/fr/univLille/cristal/shex/validation/RecursiveValidation.java
+++ shex/src/main/java/fr/univLille/cristal/shex/validation/RecursiveValidation.java
@@ -24,7 +24,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-import java.util.Stack;
 
 import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Value;
