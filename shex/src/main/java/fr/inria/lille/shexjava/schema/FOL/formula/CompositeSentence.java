11fb0e619330fddcb1a4661db1d5170b90267c93
Jérémie Dusart <jeremie.dusart@inria.fr>
2018-05-24 12:00:38 +0200

    Fusing with commons

--- shex/src/main/java/fr/inria/lille/shexjava/schema/FOL/formula/CompositeSentence.java
+++
@@ -1,7 +0,0 @@
-package fr.inria.lille.shexjava.schema.FOL.formula;
-
-import java.util.List;
-
-public interface CompositeSentence {
-	public List<Sentence> getSubSentences();
-}
--- shex/src/main/java/fr/inria/lille/shexjava/schema/FOL/formula/Quantifier.java
+++
@@ -1,5 +0,0 @@
-package fr.inria.lille.shexjava.schema.FOL.formula;
-
-public interface Quantifier {
-	public Variable getVariable();
-}
